22aa8ee09f44d99192e30c12fae9b84fc7743951
luigialejandro <50028217+luigialejandro@users.noreply.github.com>
2023-04-16 16:06:47 -0600

    Handling two particular events, i) A scrollable text within a div was built with the purpose of being able to find out the scroll bar exact position value. To achieve this I assigned the onScroll method to the (scroll) event so as to read the event object and extract the relevant info, ii) I implemented a (keyup) event into an input tag so as to capture the value of the incoming typed characters. This info is then processed at the changeName event and the result is the modification and replacement of the persons name.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   name = 'Nicolas';
@@ -24,6 +24,16 @@ export class AppComponent {
 
   increaseAge() {
     this.person.age += 1; 
+  }
+
+  onScroll(event: Event) {
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  changeName(event: Event) {
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
   }
 
 }
