e4d1c742696a4f2b8e8d91ed74f61e8c4e838d44
luigialejandro <50028217+luigialejandro@users.noreply.github.com>
2023-04-15 23:30:06 -0600

    This new file contains some basic code lines to examine how Typescript works. Some examples are i) a typed variable declaration, the appearance of an arrow function and the instance of a class supplying proper parameters (initializing variables of a particular class through the builder).

---
+++ src/app/recap.ts
@@ -0,0 +1,20 @@
+const username: string | number = 'nicobytes';
+
+const sum = (a: number, b: number) => {
+    return a + b;
+}
+
+sum(1, 2);
+
+class Person {
+    age: number;
+    lastName: string;
+
+    constructor(age: number, lastName: string) {
+        this.age = age;
+        this.lastName = lastName;
+    }
+}
+
+const nico = new Person(15, 'Molinillo');
+nico.age;
